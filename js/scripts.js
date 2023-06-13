385530498541dea3ab1cefb5bbe5d1e7ddbfb384
hirash <118464057+hIRASHnxn@users.noreply.github.com>
2023-06-13 21:34:15 +0530

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,94 @@
+/*!
+* Start Bootstrap - Agency v7.0.11 (https://startbootstrap.com/theme/agency)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+ // Dropdown on mouse hover
+ const $dropdown = $(".dropdown");
+ const $dropdownToggle = $(".dropdown-toggle");
+ const $dropdownMenu = $(".dropdown-menu");
+ const showClass = "show";
+ 
+ $(window).on("load resize", function() {
+     if (this.matchMedia("(min-width: 992px)").matches) {
+         $dropdown.hover(
+         function() {
+             const $this = $(this);
+             $this.addClass(showClass);
+             $this.find($dropdownToggle).attr("aria-expanded", "true");
+             $this.find($dropdownMenu).addClass(showClass);
+         },
+         function() {
+             const $this = $(this);
+             $this.removeClass(showClass);
+             $this.find($dropdownToggle).attr("aria-expanded", "false");
+             $this.find($dropdownMenu).removeClass(showClass);
+         }
+         );
+     } else {
+         $dropdown.off("mouseenter mouseleave");
+     }
+ });
+ 
+
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  }
